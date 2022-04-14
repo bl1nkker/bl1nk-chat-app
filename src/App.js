18472bd288e092db4071134e12187a64b962d721
bl1nkker <nurovich14@gmail.com>
2022-04-14 21:48:22 +0600

    style: mvp project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import './css/index.css'
 
 import Join from './components/Join'
 import Chat from './components/Chat'
